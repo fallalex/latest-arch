c3ff00f8499b84101bf54c678f157edd00c7497a
Alex Fall <alex@fallalex.com>
2020-08-02 11:20:23 -0400

    progress bar

--- latest-arch.py
+++ latest-arch.py
@@ -13,6 +13,7 @@ from datetime import datetime
 from distutils import util
 import sys
 import hashlib
+from time import sleep
 
 class MissingField(Exception):
     def __init__(self, expression):
@@ -20,14 +21,15 @@ class MissingField(Exception):
 
 class latestArch:
     def __init__(self):
+        # TODO - load from configuration or commandline
+        self.cwd = pathlib.Path(os.getcwd())
+        self.script_dir = pathlib.Path(os.path.realpath(__file__)).parent
         self.arch_url = 'https://www.archlinux.org'
         self.releases_endpoint = '/releng/releases'
         self.releases_url = self.arch_url + self.releases_endpoint
-        self.cwd = pathlib.Path(os.getcwd())
-        self.script_dir = pathlib.Path(os.path.realpath(__file__)).parent
         self.iso_info_path = self.cwd / '.arch-iso'
         self.torrent_path = self.cwd / 'arch.torrent'
-        self.bitclient =Client('http://127.0.0.1:8080/')
+        self.bitclient = Client('http://127.0.0.1:8080/')
 
         self.get_release_url()
         self.get_iso_info()
@@ -36,6 +38,7 @@ class latestArch:
         print(tabulate(self.iso_info.items()))
         self.iso_path = self.cwd / self.iso_info['file_name']
         self.get_torrent()
+        self.poll_download()
         print(self.file_hash())
 
     def get_release_url(self):
@@ -90,31 +93,67 @@ class latestArch:
             print("Not downloading")
 
         self.iso_path = pathlib.Path(self.bitclient.get_default_save_path()) / self.iso_info['file_name']
+        assert(self.torrent_status() in (0,1))
 
+
+    def bitclient_status(self):
+        try:
+            return (self.bitclient.qbittorrent_version, self.bitclient.api_version)
+        except:
+            return None
 
     def torrent_status(self):
         """
         0 = complete
         1 = not complete
-        2 = bad connection/no hash
+        2 = no hash
+        3 = client not responding
         """
-        # monitor download
-        # check if reachable
-        self.bitclient.api_version
-        self.bitclient.qbittorrent_version
         try:
-            torrent_info = self.bitclient.get_torrent(self.iso_info['info_hash'])
+            self.torrent_info = self.bitclient.get_torrent(self.iso_info['info_hash'])
         except Exception as e:
-            # likely the torrent is not loaded
-            # could also be no connection
+            if not self.bitclient_status():
+                return 3
             return 2
         # '-1' if not complete, otherwise epoch time
-        if int(torrent_info['completion_date']) >= 0:
+        if int(self.torrent_info['completion_date']) >= 0:
             return 0
+        # expected fields exist
+        for k in ('completion_date', 'dl_speed', 'eta', 'pieces_have', 'pieces_num', 'time_elapsed'):
+            if not k in self.torrent_info:
+                raise MissingField(k)
         return 1
 
+    def poll_download(self):
+        # check that downloaded is increasing between polls
+        # set retry limits for client and torrent
+        # set polling rate
+        # attempt to reload torrent if in bad state
+        #  - if not found and client up re-add
+        #  - if not downloading force start
+        #  - try bumping priority up
+        #  - delete and re-add
+        with tqdm(total=self.torrent_info['pieces_num']) as pbar:
+            pbar.n = self.torrent_info['pieces_have']
+            pbar.refresh()
+            while True:
+                status = self.torrent_status()
+                if not status:
+                    pbar.close()
+                    return
+                elif status == 1:
+                    pbar.n = self.torrent_info['pieces_have']
+                    pbar.refresh()
+                elif status == 2:
+                    pass
+                    # print('no hash')
+                elif status == 3:
+                    pass
+                    # print('no client')
+                sleep(3)
+
     def file_hash(self):
-        chunk = 65536  # 64kb
+        chunk = 65536  # 64kB
         md5 = hashlib.md5()
         sha1 = hashlib.sha1()
         with open(self.iso_path, 'rb') as f:
